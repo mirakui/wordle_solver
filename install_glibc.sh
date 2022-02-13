ee912c7fa9c674d1087643cee988d4d74f1a16f0
Issei Naruta <naruta@cookpad.com>
2022-02-14 00:16:56 +0900

    Suppress logs 2

--- install_glibc.sh
+++ install_glibc.sh
@@ -3,9 +3,9 @@
 yum install -y wget
 wget -q -P /tmp/ https://ftp.gnu.org/gnu/glibc/glibc-2.18.tar.gz
 cd /tmp
-tar xvf glibc-2.18.tar.gz 2>/dev/null
+tar xvf glibc-2.18.tar.gz >/dev/null 2>&1
 cd /tmp/glibc-2.18
 mkdir build; cd build
 ../configure --prefix=/usr --disable-profile --enable-add-ons --with-headers=/usr/include --with-binutils=/usr/bin
-make 2>/dev/null
+make >/dev/null 2>&1
 make install
