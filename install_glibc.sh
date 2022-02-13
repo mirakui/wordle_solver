3fb1d92411157a6aecdc1775c5e81054cae23604
Issei Naruta <naruta@cookpad.com>
2022-02-13 23:46:10 +0900

    Update

--- install_glibc.sh
+++ install_glibc.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash +xe
 
-yum install -y unzip wget
+yum install -y wget
 wget -P /tmp/ https://ftp.gnu.org/gnu/glibc/glibc-2.18.tar.gz
 cd /tmp
 tar xvf glibc-2.18.tar.gz
-cd glibc-2.18
+cd /tmp/glibc-2.18
+mkdir build; cd build
 ../configure --prefix=/usr --disable-profile --enable-add-ons --with-headers=/usr/include --with-binutils=/usr/bin
+cd ..
+make
+make install
